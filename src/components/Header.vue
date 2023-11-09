a836323255a2fbcf4ecf60c0b8664a7c3885ab69
Mugisha Regis <mugisharegis72@gmail.com>
2023-11-09 18:38:13 +0200

    implemented the template

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Expense Tracker</h2>
+    </div>
+</template>
+<script setup></script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }}
+            <button class="delete-btn">X</button>
+        </li>
+    </ul>
+</template>
+<script setup>
+    const transactions = [
+        { id: 1 , text: 'Flower', amount: -19.99},
+        { id: 2 , text: 'Salary', amount: 299.97},
+    ]
+
+</script>
